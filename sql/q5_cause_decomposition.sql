2f896660da107b0f34046d2afe0a7e920e9b1a91
samuxx99 <aglieco39@gmail.com>
2026-06-19 07:52:09 +0200

    Fix SQL table reference in cause decomposition query

--- sql/q5_cause_decomposition.sql
+++ sql/q5_cause_decomposition.sql
@@ -1,7 +1,7 @@
 SELECT 
   'Traffic Signal' AS infrastructure_element,
   ROUND(100 * COUNTIF(Traffic_Signal = true) / COUNT(*), 2) AS impact_percentage
-FROM `accidents_2022.accidents`
+FROM `accidents_2022.accidents_2022`
 
 UNION ALL
 
